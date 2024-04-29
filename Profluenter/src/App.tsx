a7f72291bcc63bfa1c881fc4245989d9563d027f
Epic-Container <smpdrive0@gmail.com>
2024-04-29 22:02:09 +0700

    Menambahkan dark mode

--- Profluenter/src/App.tsx
+++ Profluenter/src/App.tsx
@@ -1,11 +1,34 @@
-//import components disini ya
-import Navbar from './Components/Navbar'
+//import component disini
+import { useState } from 'react';
+import Navbar from './Components/Navbar';
+interface theme {
+  true: string,
+  false: string
+}
+interface title {
+  true: string,
+  false: string
+}
 function App() {
+  const [toggleDark, toggleDarkSet] = useState(true);
+  const darkMode = () => {
+    toggleDarkSet(!toggleDark);
+  };
+  const darkModeButton = <button className="hover:text-white" onClick={darkMode}>o</button>
+  const theme: theme = {
+    true: "flex justify-start gap-4 text-gray-300 bg-black p-2",
+    false: "flex justify-start gap-4 text-gray-900 bg-white p-2"
+  }
+  const title: title = {
+    true: "text-white",
+    false: "text-black"
+  }
+
   return (
     <main>
-      <Navbar/>
+      <Navbar theme={theme[`${toggleDark}`]} title={title[`${toggleDark}`]} button={darkModeButton}/>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- Profluenter/src/Components/Navbar.tsx
+++ Profluenter/src/Components/Navbar.tsx
@@ -1,22 +1,22 @@
-import {useState, useRef, useEffect} from 'react'
-const Navbar = () => {
-  const [dark, darkSet] = useState(false)
-  const darkMode = () => {
-    
-  }
+import React from "react";
+
+interface NavbarProps {
+  theme:string;
+  button:JSX.Element;
+  title:string;
+}
+const Navbar:React.FC<NavbarProps> = ({theme, button, title}) => {
+
   return (
-    <div className="">
-      <div className="flex justify-start gap-4 text-gray-300 bg-[black] p-2">
-        <span className="text-white">Profluenter</span>
-        <button className="hover:text-white">File</button>
-        <button className="hover:text-white">Setting</button>
-        <button className="hover:text-white">Tools</button>
-        <button className="hover:text-white">Docs</button>
-      </div>
-      <div>
-        <button className="">o</button>
-      </div>
+    <div className={theme}>
+      <span className={title}>Profluenter</span>
+      <button className="hover:text-white">File</button>
+      <button className="hover:text-white">Setting</button>
+      <button className="hover:text-white">Tools</button>
+      <button className="hover:text-white">Docs</button>
+      {button}
     </div>
   );
 };
+
 export default Navbar;
