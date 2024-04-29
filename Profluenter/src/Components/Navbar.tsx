8f4b46dcf15681b8fae8bed4c7e965a04a8a5c27
Epic-Container <smpdrive0@gmail.com>
2024-04-29 09:46:09 +0700

    mengubah nav

--- Profluenter/src/Components/Navbar.tsx
+++ Profluenter/src/Components/Navbar.tsx
@@ -1,12 +1,21 @@
-import {useState} from 'react'
+import {useState, useRef, useEffect} from 'react'
 const Navbar = () => {
+  const [dark, darkSet] = useState(false)
+  const darkMode = () => {
+    
+  }
   return (
-    <div className="flex justify-start gap-4 text-gray-300 bg-[black] p-2">
-      <span className="text-white">Profluenter</span>
-      <button className="hover:text-white">File</button>
-      <button className="hover:text-white">Setting</button>
-      <button className="hover:text-white">Tools</button>
-      <button className="hover:text-white">Docs</button>
+    <div className="">
+      <div className="flex justify-start gap-4 text-gray-300 bg-[black] p-2">
+        <span className="text-white">Profluenter</span>
+        <button className="hover:text-white">File</button>
+        <button className="hover:text-white">Setting</button>
+        <button className="hover:text-white">Tools</button>
+        <button className="hover:text-white">Docs</button>
+      </div>
+      <div>
+        <button className="">o</button>
+      </div>
     </div>
   );
 };
