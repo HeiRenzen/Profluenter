8807b57ce1ee11af8b591c76812cbd8a48b4163d
Epic-Container <smpdrive0@gmail.com>
2024-04-30 07:21:26 +0700

    simplified code

--- Profluenter/src/App.tsx
+++ Profluenter/src/App.tsx
@@ -3,11 +3,9 @@ import { useState } from 'react';
 import Navbar from './Components/Navbar';
 interface theme {
   true: string,
-  false: string
-}
-interface title {
-  true: string,
-  false: string
+  false: string,
+  true1: string,
+  false1: string
 }
 function App() {
   const [toggleDark, toggleDarkSet] = useState(true);
@@ -17,16 +15,14 @@ function App() {
   const darkModeButton = <button className="hover:text-white" onClick={darkMode}>o</button>
   const theme: theme = {
     true: "flex justify-start gap-4 text-gray-300 bg-black p-2",
-    false: "flex justify-start gap-4 text-gray-900 bg-white p-2"
-  }
-  const title: title = {
-    true: "text-white",
-    false: "text-black"
+    false: "flex justify-start gap-4 text-gray-900 bg-white p-2",
+    true1: "text-white",
+    false2: "text-black"
   }
 
   return (
     <main>
-      <Navbar theme={theme[`${toggleDark}`]} title={title[`${toggleDark}`]} button={darkModeButton}/>
+      <Navbar theme={theme[`${toggleDark}`]} title={theme[`${toggleDark}1`]} button={darkModeButton}/>
     </main>
   );
 }
